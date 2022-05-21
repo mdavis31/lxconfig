f0a036703379674778ccc23eea8e2294d4efd021
Michael <mdavis@parrsridge.com>
2022-05-21 01:27:24 -0400

    small steppy change

--- scripts/steppy/steppy.py
+++ scripts/steppy/steppy.py
@@ -1,37 +1,43 @@
 import os, subprocess
 
+def accept_yesno (inp):
+    if inp.upper() in ['Y', 'YES', 'YE', 'YEAH' 'YUP']:
+        return 0, 'yes'
+    elif inp.upper() in ['N', 'NO', 'NAH', 'NOPE']:
+        return 0, 'no'
+    else:
+        return -1, "wrong choice"
+
+def accept_yesno_y (inp):
+    inp = inp if inp != "" else "Y"
+    return accept_yesno(inp)
+
+def accept_yesno_n (inp):
+    inp = inp if inp != "" else "N"
+    return accept_yesno(inp)
+
 def main ():
 
-    def accept_yesno_y (inp):
-        if inp.upper() in ['Y', 'YES', 'YE', 'YEAH' 'YUP'] or inp == "":
-            return 0, 'yes'
-        elif inp.upper() in ['N', 'NO', 'NAH', 'NOPE']:
-            return 0, 'no'
-        else:
-            return -1, "Wrong choice, please choose (Y/n)"
+    git_email = "mdavis@parrsridge.com"
+    git_name = "Michael Davis"
 
-    def accept_yesno_n (inp):
-        if inp.upper() in ['Y', 'YES', 'YE', 'YEAH' 'YUP']:
-            return 0, 'yes'
-        elif inp.upper() in ['N', 'NO', 'NAH', 'NOPE'] or inp == "":
-            return 0, 'no'
-        else:
-            return -1, "Wrong choice, please choose (y/N)"
-            
-
-    rc, val = prompt ("Select a choice (Y/n)", accept_yesno_n)
+    rc, val = prompt ("Would you like to load the following git info (Y/n)?\n  %s - %s" % (git_name, git_email), accept_yesno)
     if val == 'yes':
-        print "Okay, running code.."
+        print (run_command (f'git config --global user.email "{git_email}"'))
+        print (run_command (f'git config --global user.name "{git_name}"'))
     
 
-    print (rc, data)
+    #print (run_command (f'ssh-keygen -t ed25519 -C "{git_email}"'))
+    #print (run_command (f'sudo ssh-add ~/.ssh/id_ed25519'))
+    
+    print (rc, val)
 
 def prompt(prompt, accepted_method):
-    rc = None
-    while rc == None:
-        inp = input(prompt + "=> ")
-        rc = accepted_method(inp.strip())
-    return rc
+    rc = -1
+    while rc == -1:
+        inp = input(prompt + " => ")
+        rc, val = accepted_method(inp.strip())
+    return rc, val
 
 
 def run_command (cmd, bytes=False):
